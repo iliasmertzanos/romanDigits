ca82652ff598245c7077c0bfb0904494617d7e16
iliasmertzanos <mertzanos@gmail.com>
2019-11-03 22:44:50 +0100

    final solution for Roman Numerals

--- src/main/kotlin/RomanNumeral.kt
+++ src/main/kotlin/RomanNumeral.kt
@@ -5,19 +5,35 @@ class RomanNumeral {
 			var romanDigits:String=""
 			
 			var listOfRomanDigits=listOf(
-					Pair(1,"I"),
+				Pair(1,"I"),
+				Pair(4,"IV"),
 				Pair(5,"V"),
+				Pair(9,"IX"),
 				Pair(10,"X"),
+				Pair(40,"XL"),
 				Pair(50,"L"),
+				Pair(90,"XC"),
 				Pair(100,"C"),
+				Pair(400,"CD"),
 				Pair(500,"D"),
+				Pair(900,"CM"),
 				Pair(1000,"M")				
 			)
 			
-			while(inputNr!=0){
-//				var dividor=listOfRomanDigits.findLast{it.first.compareTo(inputNr)==1 && ((it.first/inputNr) =<3 )}
-//				var modulo=
+			if(inputNr!=0){
+				var basePair =listOfRomanDigits.findLast{it.first.compareTo(inputNr)<1}
+				System.out.println("  FOUND BASE :"+ basePair +" for input "+inputNr);
+				
+				if(basePair!=null){
+					var quotient=inputNr/basePair.first;
+					var modulo=inputNr%basePair.first;
+					System.out.println(" QUOTIENT :"+ quotient +" MODULO "+modulo);
+					return basePair.second.repeat(quotient) + value(modulo);
+				}
+			}else{
+				return "";
 			}
+			
 			return "";
 		}
 	}
